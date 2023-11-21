e1e944aea9285be311fb6c688f219fc0a3e51916
hanbeulYou <hanbeul.you@gmail.com>
2023-11-21 22:15:13 +0900

    lecture-1 refactor: bottleneck 코드 최적화

--- lecture-1/src/components/Article/index.js
+++ lecture-1/src/components/Article/index.js
@@ -17,36 +17,41 @@ function getParametersForUnsplash({ width, height, quality, format }) {
  * (Markdown으로 된 문자열의 특수문자를 제거하기 위함)
  * */
 function removeSpecialCharacter(str) {
-  const removeCharacters = [
-    "#",
-    "_",
-    "*",
-    "~",
-    "&",
-    ";",
-    "!",
-    "[",
-    "]",
-    "`",
-    ">",
-    "\n",
-    "=",
-    "-",
-  ];
-  let _str = str;
-  let i = 0,
-    j = 0;
+  // const removeCharacters = [
+  //   "#",
+  //   "_",
+  //   "*",
+  //   "~",
+  //   "&",
+  //   ";",
+  //   "!",
+  //   "[",
+  //   "]",
+  //   "`",
+  //   ">",
+  //   "\n",
+  //   "=",
+  //   "-",
+  // ];
+  // let _str = str;
+  // let i = 0,
+  //   j = 0;
 
-  for (i = 0; i < removeCharacters.length; i++) {
-    j = 0;
-    while (j < _str.length) {
-      if (_str[j] === removeCharacters[i]) {
-        _str = _str.substring(0, j).concat(_str.substring(j + 1));
-        continue;
-      }
-      j++;
-    }
-  }
+  // for (i = 0; i < removeCharacters.length; i++) {
+  //   j = 0;
+  //   while (j < _str.length) {
+  //     if (_str[j] === removeCharacters[i]) {
+  //       _str = _str.substring(0, j).concat(_str.substring(j + 1));
+  //       continue;
+  //     }
+  //     j++;
+  //   }
+  // }
+
+  // 모든 문자열을 검사할 필요 없이 앞에 나오는 내용(약 200자)만 최적화 하면 됨
+  // 추가적으로 2중 for문 대신 replace 함수 & 정규식 활용
+  let _str = str.substring(0, 300);
+  _str.replace(/[\#\_\*\~\&\;\!\[\]\`\>\n\=\-]/g, "");
 
   return _str;
 }
